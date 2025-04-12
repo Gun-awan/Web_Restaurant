0bc92a41e53a79743d27e6a2fe3d28e602a892c5
Ahmad Gunawan <ahmadgunawan22022000@gmail.com>
2025-04-12 20:51:00 +0700

    crud menu

---
+++ tambah_menu.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Menu</title>
+</head>
+<body>
+
+    <center><p>Data Menu</p></center>
+    <center><a href="tambah.php">+ &nbsp; Tambah Produk </a></center>
+    <table>
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Menu</th>
+                <th>Deskripsi</th>
+                <th>Harga</th>
+                <th>Gambar</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+    </table>
+    
+</body>
+</html>
